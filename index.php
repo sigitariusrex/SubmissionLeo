73c7aa7b5670ad762b8d271eaa78518a8293a7f3
sigitariusrex <47717829+sigitariusrex@users.noreply.github.com>
2019-05-16 21:11:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     $db = "submissiondb";
 
     try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db, $user, $pass");
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
@@ -45,7 +45,7 @@
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
+            $sql_insert = "INSERT INTO [dbo].[Registration] (name, email, job, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
@@ -60,7 +60,7 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM Registration";
+            $sql_select = "SELECT * FROM [dbo].[Registration]";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
